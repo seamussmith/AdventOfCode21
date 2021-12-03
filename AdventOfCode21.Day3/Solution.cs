ad6757b003a2616724e0e230cf6c95b69b1daa5d
seamussmith <student.141504@worcesterschools.net>
2021-12-03 05:58:54 -0500

    cormit

--- AdventOfCode21.Day3/Solution.cs
+++ AdventOfCode21.Day3/Solution.cs
@@ -3,16 +3,69 @@ namespace AdventOfCode21.Day3;
 
 public class Solution
 {
+    readonly IReadOnlyList<string> bits;
     public Solution(string input)
     {
-        // Parse your input and preferably store it
+        bits = input.Split("\n").Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
     }
     public long Solution1()
     {
-        throw new NotImplementedException("Solution 1 is not implemented yet");
+        var comm = new long[bits[0].Length];
+        var majorBits = bits.Count() / 2;
+        foreach (var b in bits)
+        {
+            for (var i = 0; i != b.Length; ++i)
+                if (b[i] == '1')
+                    comm[i] += 1;
+        }
+        var gamma = Convert.ToInt64(comm.Aggregate("", (a, b) => a + (b >= majorBits ? "1" : "0")), 2);
+        var epsilon = Convert.ToInt64(comm.Aggregate("", (a, b) => a + (b <= majorBits ? "1" : "0")), 2);
+
+        return gamma * epsilon;
+    }
+    public long[] GetCommonBits(IReadOnlyList<string> arr)
+    {
+        var comm = new long[arr[0].Length];
+        var majorBits = arr.Count() / 2;
+        foreach (var b in arr)
+        {
+            for (var i = 0; i != b.Length; ++i)
+                if (b[i] == '1')
+                    comm[i] += 1;
+        }
+        return comm;
     }
     public long Solution2()
     {
-        throw new NotImplementedException("Solution 2 is not implemented yet");
+        long o2rating;
+        long co2rating;
+        var nums = bits.Select(x => x).ToList();
+        var iter = 0;
+        while (nums.Count() != 1)
+        {
+            var comm = GetCommonBits(nums);
+            var majorBits = nums.Count() / 2;
+            if (comm[iter] > majorBits)
+                nums = nums.Where(x => x[iter] == '1').ToList();
+            else
+                nums = nums.Where(x => x[iter] == '0').ToList();
+            ++iter;
+        }
+        o2rating = Convert.ToInt64(nums.First(), 2);
+        nums = bits.Select(x => x).ToList();
+        iter = 0;
+        while (nums.Count() != 1)
+        {
+            var comm = GetCommonBits(nums);
+            var majorBits = nums.Count() / 2;
+            if (comm[iter] > majorBits)
+                nums = nums.Where(x => x[iter] != '1').ToList();
+            else
+                nums = nums.Where(x => x[iter] != '0').ToList();
+            ++iter;
+        }
+        co2rating = Convert.ToInt64(nums.First(), 2);
+
+        return o2rating * co2rating;
     }
 }
